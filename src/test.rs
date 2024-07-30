0ff33bc830af4266d3ab5c1f8b78af880fac9f80
Peter Bjorklund <piot@hotmail.com>
2024-07-30 22:20:48 +0200

    test: add test

--- src/test.rs
+++ src/test.rs
@@ -1,15 +1,17 @@
-use crate::ray_vs_rect;
 #[cfg(test)]
-use fixed32::Fp;
-use fixed32_math::{Rect, Vector};
+mod tests {
+    use crate::ray_vs_rect;
+    use fixed32::Fp;
+    use fixed32_math::{Rect, Vector};
 
-#[test]
-fn test_ray_vs_rect() {
-    let ray_origin = Vector::from((1, 2));
-    let ray_direction = Vector::from((3, 4));
-    let target_rect = Rect::from((5, 6, 7, 8));
+    #[test]
+    fn test_ray_vs_rect() {
+        let ray_origin = Vector::from((1, 2));
+        let ray_direction = Vector::from((3, 4));
+        let target_rect = Rect::from((5, 6, 7, 8));
 
-    let collision_result = ray_vs_rect(ray_origin, ray_direction, target_rect);
-    let ray_intersect = collision_result.expect("should have intersected");
-    assert_eq!(ray_intersect.closest_time, Fp::from(1.33332));
+        let collision_result = ray_vs_rect(ray_origin, ray_direction, target_rect);
+        let ray_intersect = collision_result.expect("should have intersected");
+        assert_eq!(ray_intersect.closest_time, Fp::from(1.33332));
+    }
 }
